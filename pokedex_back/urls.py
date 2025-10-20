1a05cad0255bb9ce37515bb2d95834992e5a6ee2
AlessandraRomero <alessandraromeiro098@gmail.com>
2025-10-20 11:54:16 -0400

    Adiciona rota raiz '/' para status da API

--- pokedex_back/urls.py
+++ pokedex_back/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+def home(request):
+    return JsonResponse({"status": "ok", "message": "Pokedex API online"})
+
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('core.urls')),
 ]
+
+
